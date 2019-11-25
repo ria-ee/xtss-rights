0476ba50c03db0038bb6774577ea6e8f0923f9a4
Vitali Stupin <vitali.stupin@ria.ee>
2019-11-25 09:48:30 +0200

    Separate db schema and rights creation scripts

--- db.sql
+++ db.sql
@@ -138,14 +138,3 @@ DROP TRIGGER IF EXISTS check_right on rights."right";
 CREATE TRIGGER check_right
 BEFORE INSERT OR UPDATE ON rights."right"
 FOR EACH ROW EXECUTE PROCEDURE rights.check_right();
-
-CREATE ROLE rights_app WITH LOGIN;
-GRANT CONNECT ON DATABASE rights TO rights_app;
-GRANT USAGE ON SCHEMA rights TO rights_app;
-GRANT INSERT ON rights.change_log TO rights_app;
-GRANT SELECT, INSERT, UPDATE ON rights.organization TO rights_app;
-GRANT SELECT, INSERT, UPDATE ON rights.person TO rights_app;
-GRANT SELECT, INSERT, UPDATE ON rights."right" TO rights_app;
-GRANT USAGE ON SEQUENCE rights.organization_id_seq TO rights_app;
-GRANT USAGE ON SEQUENCE rights.person_id_seq TO rights_app;
-GRANT USAGE ON SEQUENCE rights.right_id_seq TO rights_app;
