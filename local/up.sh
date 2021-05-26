324197faea68e38db042a5d5eb2d99f6e62d9b47
Vitali Stupin <vitali.stupin@ria.ee>
2021-05-26 10:44:13 +0300

    Adding liquibase support

--- local/up.sh
+++ local/up.sh
@@ -6,3 +6,11 @@ set -x
 bash prepare.sh
 docker-compose build
 docker-compose up -d
+
+# Giving DB some time to start before liquibase step
+for i in {1..30}; do
+  (echo > /dev/tcp/localhost/5432) >/dev/null 2>&1 && break
+  sleep 1
+done
+
+docker run --rm --net local_default -v $(pwd)/../liquibase:/liquibase/changelog liquibase/liquibase --defaultsFile=/liquibase/changelog/liquibase.local.properties update
