35a20093e02e84442f8d441ad68bc2d20ad2db3b
Vitali Stupin <vitali.stupin@ria.ee>
2023-03-08 14:11:01 +0200

    Update logging examples

---
+++ local/load_test.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+for thread in {1..20}; do
+  for repeat in {1..100}; do
+    # Get status
+    curl -s localhost:5080/status > /dev/null
+    # Set right
+    curl -s -XPOST -d '{"organization":{"code":"'${thread}'","name":"Org 0"},"person":{"code":"'${repeat}'","first_name":"Firstname","last_name":"Lastname"},"right":{"right_type":"RIGHT1"}}' -H 'X-Ssl-Client-S-Dn: OU=XTSS,O=RIA,C=EE' localhost:5080/set-right > /dev/null
+    # Get rights
+    curl -s -XPOST -d '{"organizations": ["'${thread}'"],"persons": ["'${repeat}'"],"only_valid": false,"limit": 2}' -H 'X-Ssl-Client-S-Dn: OU=XTSS,O=RIA,C=EE' localhost:5080/rights > /dev/null
+  done &
+done
+wait
