97676edca3695e53a5c9d584fa7fd822e7a162f8
Vitali Stupin <vitali.stupin@ria.ee>
2023-04-26 15:06:18 +0300

    Updating dependencies
    * Updating sonar config
    * Fixing code smells

--- local/load_test.sh
+++ local/load_test.sh
@@ -5,9 +5,9 @@ for thread in {1..20}; do
     # Get status
     curl -s localhost:5080/status > /dev/null
     # Set right
-    curl -s -XPOST -d '{"organization":{"code":"'${thread}'","name":"Org 0"},"person":{"code":"'${repeat}'","first_name":"Firstname","last_name":"Lastname"},"right":{"right_type":"RIGHT1"}}' -H 'X-Ssl-Client-S-Dn: OU=XTSS,O=RIA,C=EE' localhost:5080/set-right > /dev/null
+    curl -s -XPOST -d '{"organization":{"code":"'"${thread}"'","name":"Org 0"},"person":{"code":"'"${repeat}"'","first_name":"Firstname","last_name":"Lastname"},"right":{"right_type":"RIGHT1"}}' -H 'X-Ssl-Client-S-Dn: OU=XTSS,O=RIA,C=EE' localhost:5080/set-right > /dev/null
     # Get rights
-    curl -s -XPOST -d '{"organizations": ["'${thread}'"],"persons": ["'${repeat}'"],"only_valid": false,"limit": 2}' -H 'X-Ssl-Client-S-Dn: OU=XTSS,O=RIA,C=EE' localhost:5080/rights > /dev/null
+    curl -s -XPOST -d '{"organizations": ["'"${thread}"'"],"persons": ["'"${repeat}"'"],"only_valid": false,"limit": 2}' -H 'X-Ssl-Client-S-Dn: OU=XTSS,O=RIA,C=EE' localhost:5080/rights > /dev/null
   done &
 done
 wait
